0c591406fea8f1763d393a33168116f82fadda74
Scott Mabin <scott@mabez.dev>
2020-09-12 23:36:21 +0100

    Add some no_std Xtensa targets
    ESP32, ESP32S2, ESP32S3 & ESP8266.

---
+++ compiler/rustc_target/src/spec/xtensa_esp32_none_elf.rs
@@ -0,0 +1,18 @@
+use crate::spec::{Target, TargetOptions};
+
+pub fn target() -> Target {
+    Target {
+        llvm_target: "xtensa-none-elf".to_string(),
+        pointer_width: 32,
+        data_layout: "e-m:e-p:32:32-i8:8:32-i16:16:32-i64:64-n32".to_string(),
+        arch: "xtensa".to_string(),
+        
+        options: TargetOptions {
+            cpu: "esp32".to_string(),
+            linker: Some("xtensa-esp32-elf-gcc".to_string()),
+            max_atomic_width: Some(32),
+            atomic_cas: true,
+            ..super::xtensa_base::opts()
+        },
+    }
+}
