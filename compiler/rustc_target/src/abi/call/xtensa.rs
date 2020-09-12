baad779f8805ff6c2fe28d4558347b7ce2201c3b
Scott Mabin <scott@mabez.dev>
2020-09-12 23:31:14 +0100

    Teach rustc about the Xtensa call ABI.

---
+++ compiler/rustc_target/src/abi/call/xtensa.rs
@@ -0,0 +1,124 @@
+// reference: https://github.com/espressif/llvm-project/blob/e9f57cdbcf3e0a63f395e683ccfaf7c4e6e1b093/clang/lib/CodeGen/TargetInfo.cpp#L11241
+
+use crate::abi::call::{ArgAbi, FnAbi, Reg, Uniform};
+use crate::abi::{Abi, Align, FieldsShape, HasDataLayout, Size, TyAbiInterface};
+use crate::spec::HasTargetSpec;
+
+const NUM_ARG_GPRS: u64 = 6;
+const NUM_RET_ARG_GPRS: u64 = 4;
+
+fn classify_ret_ty<'a, Ty, C>(arg: &mut ArgAbi<'_, Ty>)
+where
+    Ty: TyAbiInterface<'a, C> + Copy,
+{
+    let mut arg_gprs_left = NUM_RET_ARG_GPRS;
+
+    let size = arg.layout.size.align_to(Align::from_bits(32).unwrap());
+
+    // The rules for return and argument with type size more then 4 bytes
+    // are the same, so defer to classifyArgumentType.
+    if size.bits() > 32 {
+        classify_arg_ty(arg, &mut arg_gprs_left, NUM_RET_ARG_GPRS);
+
+        // classify_arg_ty can make the arg indirect by value which is not valid for ret args
+        match arg.mode {
+            super::PassMode::Indirect { attrs: _, extra_attrs: _, ref mut on_stack } => {
+                *on_stack = false
+            }
+            _ => {}
+        }
+    } else {
+        // LLVM DefaultABIInfo::classifyReturnType
+        if arg.layout.is_aggregate() {
+            arg.make_indirect()
+        } else {
+            arg.extend_integer_width_to(32)
+        }
+    }
+}
+
+fn classify_arg_ty<'a, Ty, C>(arg: &mut ArgAbi<'_, Ty>, arg_gprs_left: &mut u64, num_gprs: u64)
+where
+    Ty: TyAbiInterface<'a, C> + Copy,
+{
+    assert!(*arg_gprs_left <= num_gprs, "GPR tracking underflow");
+
+    if arg.layout.is_zst() {
+        return; // ignore args that take no size
+    }
+
+    let size = arg.layout.size.bits();
+    let align = arg.layout.align.abi.bits();
+    let mut must_use_stack = false;
+
+    let mut required_gprs = (size + 31) / 32;
+
+    if align == 64 {
+        required_gprs += *arg_gprs_left % 2;
+    }
+
+    // Put on stack objects which are not fit to num_gprs registers,
+    // also on stack object which alignment more then 16 bytes and
+    // object with 16-byte alignment if it isn't the first argument.
+    if required_gprs > *arg_gprs_left || align > 128 || *arg_gprs_left < num_gprs && align == 128 {
+        must_use_stack = true;
+        required_gprs = *arg_gprs_left;
+    }
+    *arg_gprs_left -= required_gprs;
+
+    if must_use_stack {
+        arg.make_indirect_byval();
+    } else {
+        if arg.layout.is_aggregate()
+            && !matches!(arg.layout.abi, Abi::Vector { element: _, count: _ })
+        {
+            if size < 32 && matches!(arg.layout.fields, FieldsShape::Primitive) {
+                arg.extend_integer_width_to(32);
+            } else if size == 64 {
+                arg.cast_to(Reg::i64());
+            } else {
+                arg.cast_to(Uniform {
+                    unit: Reg::i32(),
+                    total: Size::from_bits(required_gprs * 32),
+                });
+            }
+        } else if size <= num_gprs * 32 {
+            // Aggregates which are <= num_gprs*32 will be passed in registers if possible,
+            // so coerce to integers.
+            if size <= 32 {
+                arg.cast_to(Reg::i32())
+            } else if align == 64 {
+                arg.cast_to(Uniform {
+                    unit: Reg::i64(),
+                    total: Size::from_bits((required_gprs / 2) * 64),
+                });
+            } else {
+                arg.cast_to(Uniform {
+                    unit: Reg::i32(),
+                    total: Size::from_bits(required_gprs * 32),
+                });
+            }
+        } else {
+            panic!("unhandled GPR")
+        }
+    }
+}
+
+pub fn compute_abi_info<'a, Ty, C>(_cx: &C, fn_abi: &mut FnAbi<'a, Ty>)
+where
+    Ty: TyAbiInterface<'a, C> + Copy,
+    C: HasDataLayout + HasTargetSpec,
+{
+    if !fn_abi.ret.is_ignore() {
+        classify_ret_ty(&mut fn_abi.ret);
+    }
+
+    let mut avail_gprs = NUM_ARG_GPRS;
+
+    for arg in fn_abi.args.iter_mut() {
+        if arg.is_ignore() {
+            continue;
+        }
+        classify_arg_ty(arg, &mut avail_gprs, NUM_ARG_GPRS);
+    }
+}
