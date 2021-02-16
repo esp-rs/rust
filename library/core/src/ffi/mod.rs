7b3b12581ef8d1b8467b4deef0b2a9db6e7288dd
Brian J. Tarricone <brian@tarricone.org>
2021-02-15 23:32:16 -0800

    Teach rust core about Xtensa VaListImpl

--- library/core/src/ffi/mod.rs
+++ library/core/src/ffi/mod.rs
@@ -212,6 +212,7 @@ impl fmt::Debug for c_void {
         not(target_arch = "aarch64"),
         not(target_arch = "powerpc"),
         not(target_arch = "s390x"),
+        not(target_arch = "xtensa"),
         not(target_arch = "x86_64")
     ),
     all(target_arch = "aarch64", target_vendor = "apple"),
@@ -240,6 +241,7 @@ pub struct VaListImpl<'f> {
         not(target_arch = "aarch64"),
         not(target_arch = "powerpc"),
         not(target_arch = "s390x"),
+        not(target_arch = "xtensa"),
         not(target_arch = "x86_64")
     ),
     all(target_arch = "aarch64", target_vendor = "apple"),
@@ -346,6 +348,24 @@ pub struct VaListImpl<'f> {
     _marker: PhantomData<&'f mut &'f c_void>,
 }
 
+/// xtensa ABI implementation of a `va_list`.
+#[cfg(target_arch = "xtensa")]
+#[repr(C)]
+#[derive(Debug)]
+#[unstable(
+    feature = "c_variadic",
+    reason = "the `c_variadic` feature has not been properly tested on \
+              all supported platforms",
+    issue = "44930"
+)]
+#[lang = "va_list"]
+pub struct VaListImpl<'f> {
+    stk: *mut i32,
+    reg: *mut i32,
+    ndx: i32,
+    _marker: PhantomData<&'f mut &'f i32>,
+}
+
 /// A wrapper for a `va_list`
 #[cfg_attr(not(doc), repr(transparent))] // work around https://github.com/rust-lang/rust/issues/90435
 #[derive(Debug)]
@@ -361,6 +381,7 @@ pub struct VaList<'a, 'f: 'a> {
             not(target_arch = "aarch64"),
             not(target_arch = "powerpc"),
             not(target_arch = "s390x"),
+            not(target_arch = "xtensa"),
             not(target_arch = "x86_64")
         ),
         all(target_arch = "aarch64", target_vendor = "apple"),
@@ -375,6 +396,7 @@ pub struct VaList<'a, 'f: 'a> {
             target_arch = "aarch64",
             target_arch = "powerpc",
             target_arch = "s390x",
+            target_arch = "xtensa",
             target_arch = "x86_64"
         ),
         any(not(target_arch = "aarch64"), not(target_vendor = "apple")),
@@ -392,6 +414,7 @@ pub struct VaList<'a, 'f: 'a> {
         not(target_arch = "aarch64"),
         not(target_arch = "powerpc"),
         not(target_arch = "s390x"),
+        not(target_arch = "xtensa"),
         not(target_arch = "x86_64")
     ),
     all(target_arch = "aarch64", target_vendor = "apple"),
@@ -418,6 +441,7 @@ impl<'f> VaListImpl<'f> {
         target_arch = "aarch64",
         target_arch = "powerpc",
         target_arch = "s390x",
+        target_arch = "xtensa",
         target_arch = "x86_64"
     ),
     any(not(target_arch = "aarch64"), not(target_vendor = "apple")),
