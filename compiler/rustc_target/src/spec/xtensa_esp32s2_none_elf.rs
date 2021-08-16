73eee6a5ac87414ab57f707879b6a4e03a0e1d17
Kerry Jones <kerry@iodrive.co.za>
2021-08-16 16:52:59 +0200

    Restore xtensa llvm_target tripples

--- compiler/rustc_target/src/spec/xtensa_esp32s2_none_elf.rs
+++ compiler/rustc_target/src/spec/xtensa_esp32s2_none_elf.rs
@@ -3,7 +3,7 @@ use crate::abi::Endian;
 
 pub fn target() -> Target {
     Target {
-        llvm_target: "xtensa".to_string(),
+        llvm_target: "xtensa-none-elf".to_string(),
         pointer_width: 32,
         data_layout: "e-m:e-p:32:32-i8:8:32-i16:16:32-i64:64-n32".to_string(),
         arch: "xtensa".to_string(),
