6646175190e8e3305b7e989d55305f18df3fac7c
Kerry Jones <22441572+KerryRJ@users.noreply.github.com>
2021-09-01 12:24:41 +0200

    Minor simplification of xtensa targets. (#73)
    * Simplification of xtensa targets

---
+++ compiler/rustc_target/src/spec/xtensa_base.rs
@@ -0,0 +1,28 @@
+use crate::spec::{LinkerFlavor, PanicStrategy, RelocModel, TargetOptions};
+use crate::{abi::Endian, spec::abi::Abi};
+
+pub fn opts() -> TargetOptions {
+    TargetOptions {
+        os: "none".to_string(),
+        env: String::new(),
+        vendor: String::new(),
+        endian: Endian::Little,
+        c_int_width: "32".to_string(),
+        linker_flavor: LinkerFlavor::Gcc,
+        executables: true,
+        panic_strategy: PanicStrategy::Abort,
+        relocation_model: RelocModel::Static,
+        emit_debug_gdb_scripts: false,
+        unsupported_abis: vec![
+            Abi::Stdcall { unwind: false },
+            Abi::Stdcall { unwind: true },
+            Abi::Fastcall,
+            Abi::Vectorcall,
+            Abi::Thiscall { unwind: false },
+            Abi::Thiscall { unwind: true },
+            Abi::Win64,
+            Abi::SysV64,
+        ],
+        ..Default::default()
+    }
+}
