87239d08d7b6efcdd0178c57136d5bf4e988ba55
Scott Mabin <scott@mabez.dev>
2022-09-08 15:13:35 +0100

    WORKING

--- compiler/rustc_target/src/abi/call/xtensa.rs
+++ compiler/rustc_target/src/abi/call/xtensa.rs
@@ -1,113 +1,95 @@
-// reference: https://github.com/espressif/llvm-project/blob/e9f57cdbcf3e0a63f395e683ccfaf7c4e6e1b093/clang/lib/CodeGen/TargetInfo.cpp#L11241
+// reference: https://github.com/MabezDev/llvm-project/blob/xtensa_release_9.0.1_with_rust_patches-31-05-2020-cherry-pick/clang/lib/CodeGen/TargetInfo.cpp#L9668-L9767
 
 use crate::abi::call::{ArgAbi, FnAbi, Reg, Uniform};
-use crate::abi::{Abi, Align, HasDataLayout, Size, TyAbiInterface};
+use crate::abi::{Abi, HasDataLayout, Size, TyAbiInterface};
 use crate::spec::HasTargetSpec;
 
 const NUM_ARG_GPRS: u64 = 6;
-const NUM_RET_ARG_GPRS: u64 = 4;
+const NUM_RET_GPRS: u64 = 4;
+const MAX_ARG_IN_REGS_SIZE: u64 = 6 * 32;
+const MAX_RET_IN_REGS_SIZE: u64 = 4 * 32;
 
-fn classify_ret_ty<'a, Ty, C>(cx: &C, arg: &mut ArgAbi<'_, Ty>)
+fn classify_ret_ty<'a, Ty, C>(arg: &mut ArgAbi<'_, Ty>, xlen: u64)
 where
     Ty: TyAbiInterface<'a, C> + Copy,
-    C: HasDataLayout + HasTargetSpec,
 {
-    let mut arg_gprs_left = NUM_RET_ARG_GPRS;
-
-    let size = arg.layout.size.align_to(Align::from_bits(32).unwrap());
-
-    // The rules for return and argument with type size more then 4 bytes
-    // are the same, so defer to classifyArgumentType.
-    if size.bits() > 32 {
-        classify_arg_ty(cx, arg, &mut arg_gprs_left, NUM_RET_ARG_GPRS);
-
-        // classify_arg_ty can make the arg indirect by value which is not valid for ret args
-        match arg.mode {
-            super::PassMode::Indirect { attrs: _, extra_attrs: _, ref mut on_stack } => {
-                *on_stack = false
-            }
-            _ => {}
-        }
-    } else {
-        // Based on LLVM DefaultABIInfo::classifyReturnType
-        if arg.layout.is_aggregate() {
-            arg.make_indirect();
-        } else if !is_xtensa_aggregate(arg) {
-            arg.extend_integer_width_to(32);
-        } else {
-            arg.cast_to(Reg::i32());
-        }
+    if arg.is_ignore() {
+        return;
     }
+
+    // The rules for return and argument types are the same,
+    // so defer to `classify_arg_ty`.
+    let mut arg_gprs_left = NUM_RET_GPRS;
+    classify_arg_ty(arg, xlen, &mut arg_gprs_left, MAX_RET_IN_REGS_SIZE);
 }
 
-fn classify_arg_ty<'a, Ty, C>(
-    cx: &C,
-    arg: &mut ArgAbi<'_, Ty>,
-    arg_gprs_left: &mut u64,
-    num_gprs: u64,
-) where
+fn classify_arg_ty<'a, Ty, C>(arg: &mut ArgAbi<'_, Ty>, xlen: u64, arg_gprs_left: &mut u64, max_size: u64)
+where
     Ty: TyAbiInterface<'a, C> + Copy,
-    C: HasDataLayout + HasTargetSpec,
 {
-    assert!(*arg_gprs_left <= num_gprs, "GPR tracking underflow");
+    assert!(*arg_gprs_left <= NUM_ARG_GPRS, "Arg GPR tracking underflow");
 
+    // Ignore empty structs/unions.
     if arg.layout.is_zst() {
-        return; // ignore args that take no size
+        return;
     }
 
     let size = arg.layout.size.bits();
-    let align = arg.layout.align.abi.bits();
+    let needed_align = arg.layout.align.abi.bits();
     let mut must_use_stack = false;
 
-    let mut required_gprs = (size + 31) / 32;
+    // Determine the number of GPRs needed to pass the current argument
+    // according to the ABI. 2*XLen-aligned varargs are passed in "aligned"
+    // register pairs, so may consume 3 registers.
+    let mut needed_arg_gprs = 1u64;
 
-    if align == 64 {
-        required_gprs += *arg_gprs_left % 2;
+    if needed_align == 2 * xlen {
+        needed_arg_gprs = 2 + (*arg_gprs_left % 2);
+    } else if size > xlen && size <= max_size {
+        needed_arg_gprs = (size + xlen - 1) / xlen;
     }
 
-    // Put on stack objects which are not fit to num_gprs registers,
-    // also on stack object which alignment more then 16 bytes and
-    // object with 16-byte alignment if it isn't the first argument.
-    if required_gprs > *arg_gprs_left || align > 128 || *arg_gprs_left < num_gprs && align == 128 {
+    if needed_arg_gprs > *arg_gprs_left {
         must_use_stack = true;
-        required_gprs = *arg_gprs_left;
+        needed_arg_gprs = *arg_gprs_left;
     }
-    *arg_gprs_left -= required_gprs;
+    *arg_gprs_left -= needed_arg_gprs;
 
-    if must_use_stack {
-        arg.make_indirect_byval();
-    } else {
-        if size < 32 && !is_xtensa_aggregate(arg) {
-            arg.extend_integer_width_to(32); // ints should be extended
-        } else if size <= 32 {
-            arg.cast_to(Reg::i32()); // else cast to a single register
+    if !arg.layout.is_aggregate() && !matches!(arg.layout.abi, Abi::Vector { .. }) {
+        // All integral types are promoted to `xlen`
+        // width, unless passed on the stack.
+        if size < xlen && !must_use_stack {
+            arg.extend_integer_width_to(xlen);
+            return;
+        }
+
+        return; // let LLVM backend handle integral types >= xlen
+    }
+
+    // Aggregates which are <= max_size will be passed in
+    // registers if possible, so coerce to integers.
+    if needed_arg_gprs * 32 <= max_size && !must_use_stack {
+        let alignment = arg.layout.align.abi.bits();
+
+        // Use a single `xlen` int if possible, 2 * `xlen` if 2 * `xlen` alignment
+        // is required, and a 2-element `xlen` array if only `xlen` alignment is
+        // required.
+        if size <= xlen {
+            arg.cast_to(Reg::i32());
+            return;
         } else {
-            // finally, keep proper alignment in mind for larger primitives and aggregates
-            let reg = if align >= 64 {
-                if size != required_gprs * 32 {
-                    assert!(align >= 64, "{}bit align needs different padding", align);
-                    arg.pad_with(Reg::i32());
-                }
+            let reg = if alignment == 2 * xlen {
                 Reg::i64()
             } else {
                 Reg::i32()
             };
-
-            match size {
-                64 => arg.cast_to(Reg::i64()),
-                128 => arg.cast_to(Reg::i128()),
-                _ => {
-                    let array = Uniform { unit: reg, total: Size::from_bits(size) };
-                    assert_eq!(
-                        array.align(cx).bits(),
-                        align,
-                        "alignment incorrect for type with size = {size}"
-                    );
-                    arg.cast_to(array)
-                }
-            };
+            let total = Size::from_bits(((size + xlen - 1) / xlen) * xlen);
+            arg.cast_to(Uniform { unit: reg, total });
+            return;
         }
     }
+
+    arg.make_indirect_byval();
 }
 
 pub fn compute_abi_info<'a, Ty, C>(cx: &C, fn_abi: &mut FnAbi<'a, Ty>)
@@ -115,23 +97,21 @@ where
     Ty: TyAbiInterface<'a, C> + Copy,
     C: HasDataLayout + HasTargetSpec,
 {
+    let xlen = cx.data_layout().pointer_size.bits();
+
     if !fn_abi.ret.is_ignore() {
-        classify_ret_ty(cx, &mut fn_abi.ret);
+        classify_ret_ty(&mut fn_abi.ret, xlen);
     }
 
-    let mut avail_gprs = NUM_ARG_GPRS;
+    let is_ret_indirect =
+        fn_abi.ret.is_indirect() || fn_abi.ret.layout.size.bits() > MAX_RET_IN_REGS_SIZE;
 
-    for arg in fn_abi.args.iter_mut() {
+    let mut arg_gprs_left = if is_ret_indirect { NUM_ARG_GPRS - 1 } else { NUM_ARG_GPRS };
+
+    for arg in &mut fn_abi.args {
         if arg.is_ignore() {
             continue;
         }
-        classify_arg_ty(cx, arg, &mut avail_gprs, NUM_ARG_GPRS);
-    }
-}
-
-fn is_xtensa_aggregate<'a, Ty>(arg: &ArgAbi<'a, Ty>) -> bool {
-    match arg.layout.abi {
-        Abi::Vector { .. } => true,
-        _ => arg.layout.is_aggregate(),
+        classify_arg_ty(arg, xlen, &mut arg_gprs_left, MAX_ARG_IN_REGS_SIZE);
     }
 }
