a41daf6b436c637ecbb158d26d952738f2a934d2
Scott Mabin <scott@mabez.dev>
2022-11-02 11:28:18 +0000

    Enable Xtensa codegen for rustc_codegen_gcc
    * Updates uses of object::Architecture within the compiler

--- compiler/rustc_codegen_ssa/src/back/metadata.rs
+++ compiler/rustc_codegen_ssa/src/back/metadata.rs
@@ -217,6 +217,7 @@ pub(crate) fn create_object_file(sess: &Session) -> Option<write::Object<'static
         "loongarch64" => (Architecture::LoongArch64, None),
         "csky" => (Architecture::Csky, None),
         "arm64ec" => (Architecture::Aarch64, Some(SubArchitecture::Arm64EC)),
+        "xtensa" => (Architecture::Xtensa, None),
         // Unsupported architecture.
         _ => return None,
     };
